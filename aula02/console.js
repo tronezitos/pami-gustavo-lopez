fa22bdb06d6a6ed22ea4742b1dccdf496d31d677
Gustavo Lopez <gugianelli@gmail.com>
2025-02-20 08:28:04 -0300

    Aula sobre tipo de variaveis

---
+++ aula02/console.js
@@ -0,0 +1,28 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEisso = "?";
+let nome_do_aluno = "Gustavo"
+// Alguns tipos de variaveis
+
+console.log('Tipo var:', numero);
+// Ei, galera, vocês sabiam que, em termos de reprodução entre humanos do sexo masculino e Pokémon do sexo feminino, Vaporeon é o Pokémon mais compatível para humanos? Não só eles estão no grupo de ovos de campo, que é composto principalmente por mamíferos, Vaporeon tem uma média de 3'03" de altura e 63,9 libras, o que significa que eles são grandes o suficiente para lidar com pênis humanos, e com suas impressionantes estatísticas base para HP e acesso a Acid Armor, você pode ser bruto com um. Devido à sua biologia principalmente aquática, não tenho dúvidas de que um Vaporeon excitado seria incrivelmente molhado, tão molhado que você poderia facilmente fazer sexo com um por horas sem ficar dolorido. Eles também podem aprender os movimentos Attract, Baby-Doll Eyes, Captivate, Charm e Tail Whip, além de não ter pelos para esconder os mamilos, então seria incrivelmente fácil para um te deixar no clima. Com suas habilidades Water Absorb e Hydration, eles podem facilmente se recuperar da fadiga com água suficiente. Nenhum outro Pokémon se compara a esse nível de compatibilidade. Além disso, um fato divertido, se você puxar o suficiente, você pode fazer seu Vaporeon ficar branco. Vaporeon é literalmente feito para pênis humanos. Estatística de defesa implacável + pool de HP alto + Acid Armor significa que ele pode levar pau o dia todo, de todos os formatos e tamanhos e ainda querer mais.
+
+numero = 13;
+// FAZUELI!!!!!!!!!!!!!!!!!!!!!
+
+console.log(texto + " o número agora é " + numero);
+console.log(texto,"o número agora é",numero);
+console.log(`${texto} o número agora é ${numero}`);
+// Nos vai descer pra BC
+
+texto = 69;
+// 69 e um número muito foda
+
+console.log(texto + numero);
+// Dois valores númericos com + vai fazer o sistema somar eles
+
+console.log(oQueEisso)
+oQueEisso = '!'
+console.log(oQueEisso)
+// Condigo feito pra dar erro ♥
+// Constantes não poden ter novos valores atribuidos a ela
