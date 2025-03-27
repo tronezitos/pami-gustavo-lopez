86e884ffead7e847c05db648b39a392024416bcd
Gustavo Lopez <gugianelli@gmail.com>
2025-03-27 08:27:57 -0300

    Aula 3 sobre javascript e typescript

---
+++ aula03/console.ts
@@ -0,0 +1,13 @@
+// function somar (a, b){
+//     return a + b;
+// }
+
+// console.log(somar(5, "10"))
+
+function somarComTipo(a: number, b: number): number {
+    return a + b;
+
+}
+
+console.log(somarComTipo(5, 10))
+somarComTipo("oi", "tudo bom?");
